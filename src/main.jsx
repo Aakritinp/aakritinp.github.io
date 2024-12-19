c072562f99c5e5f00775495fef34417bb8c84045
Aakriti Panthi <aakritipanthi1@gmail.com>
2024-12-19 15:47:59 -0500

    Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,9 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./component/Home";
 
-// Import Components
-import Navbar from "./component/Navbar.js";
-import Home from "./component/Home.js";
-import About from "./component/About.js";
-import Projects from "./component/Project.js";
-import Contact from "./component/Contact.js";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        {/* Navbar */}
-        <Navbar />
-
-        {/* Sections */}
-        <main>
-          <Home />
-          <About />
-          <Projects />
-          <Contact />
-        </main>
-
-        {/* Footer */}
-        <footer className="bg-pink-200 py-4 text-center text-pink-700">
-          <p>© 2024 Aakriti Panthi. All rights reserved.</p>
-          <div className="mt-2 space-x-4">
-            <a
-              href="https://github.com/yourusername"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-pink-500"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://linkedin.com/in/yourusername"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-pink-500"
-            >
-              LinkedIn
-            </a>
-            <a
-              href="https://twitter.com/yourusername"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-pink-500"
-            >
-              Twitter
-            </a>
-          </div>
-        </footer>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>
+);
